637372c1ad2a616ff2f6a1b86718ba3fc595d237
Scott Idler <scott.a.idler@gmail.com>
2018-08-29 13:59:10 -0700

    fixed up entrypoint to be like autocert's fixed up one

--- lea/api/entrypoint.sh
+++ lea/api/entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-AC_EP="$0"
-AC_UID=${AC_UID:-0}
-AC_GID=${AC_GID:-0}
-AC_USER=${AC_USER:-root}
-AC_CMD="python3 -u /usr/local/bin/gunicorn -t 120 -w 2 -b :8000 main:app"
+LEA_EP="$0"
+echo "this script is: $LEA_EP"
 
-if ! getent passwd $AC_USER 2>/dev/null; then
-    echo "creating user $AC_USER with $AC_UID:$AC_GID"
-    groupadd --gid $AC_GID $AC_USER
-    useradd --uid $AC_UID --gid $AC_GID --shell /bin/bash --no-create-home $AC_USER
-    echo -e "\n$AC_USER   ALL=(ALL) NOPASSWD: ALL\n" > /etc/sudoers
-    chown -R $AC_USER:$AC_USER /usr/src/app/
+LEA_UID=${LEA_UID:-0}
+LEA_GID=${LEA_GID:-0}
+LEA_USER=${LEA_USER:-root}
+LEA_APP_TIMEOUT=${LEA_APP_TIMEOUT:-120}
+LEA_APP_PORT=${LEA_APP_PORT:-8000}
+LEA_APP_WORKERS=${LEA_APP_WORKERS:-2}
+LEA_APP_MODULE=${LEA_APP_MODULE:-main:app}
+CMD="python3 -u /usr/local/bin/gunicorn -t $LEA_APP_TIMEOUT -w $LEA_APP_WORKERS -b :$LEA_APP_PORT $LEA_APP_MODULE"
+
+if ! getent passwd "$LEA_USER" 2>/dev/null; then
+    echo "creating user $LEA_USER with $LEA_UID:$LEA_GID"
+    groupadd --gid "$LEA_GID" "$LEA_USER"
+    useradd --uid "$LEA_UID" --gid "$LEA_GID" --shell /bin/bash --no-create-home "$LEA_USER"
+    echo -e "\n$LEA_USER   ALL=(ALL) NOPASSWD: ALL\n" > /etc/sudoers
+    chown -R "$LEA_USER:$LEA_USER" /usr/src/app/
 fi
 
-echo "executing \"$AC_CMD\" as $AC_USER"
-exec su -c "$AC_CMD" $AC_USER
+echo "executing \"$CMD\" as $LEA_USER"
+exec su -c "$CMD" "$LEA_USER"
